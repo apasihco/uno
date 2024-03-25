19ff1780f3daf8e867049f9bef75bad4480580f6
Keep Calm Stay Relax <101805283+iamnolimit@users.noreply.github.com>
2024-03-26 03:01:08 +0700

    Update internationalization.py

--- internationalization.py
+++ internationalization.py
@@ -33,16 +33,7 @@ GETTEXT_DIR = 'locales'
 class _Underscore(object):
     """Class to emulate flufl.i18n behaviour, but with plural support"""
     def __init__(self):
-        self.translators = {
-            locale: gettext.GNUTranslations(
-                open(gettext.find(
-                    GETTEXT_DOMAIN, GETTEXT_DIR, languages=[locale]
-                ), 'rb')
-            )
-            for locale
-            in available_locales.keys()
-            if locale != 'en_US'  # No translation file for en_US
-        }
+        locale = 'id_ID'
         self.locale_stack = list()
 
     def push(self, locale):

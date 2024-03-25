8da488faf74a2379d052084369928da19a479b15
Keep Calm Stay Relax <101805283+iamnolimit@users.noreply.github.com>
2024-03-26 03:12:10 +0700

    Update internationalization.py

--- internationalization.py
+++ internationalization.py
@@ -34,7 +34,14 @@ class _Underscore(object):
     """Class to emulate flufl.i18n behaviour, but with plural support"""
     def __init__(self):
         self.translators = {
-            locale: 'id_ID'
+            locale: gettext.GNUTranslations(
+                open(gettext.find(
+                    GETTEXT_DOMAIN, GETTEXT_DIR, languages=[locale]
+                ), 'rb')
+            )
+            for locale
+            in available_locales.keys()
+            if locale != 'en_US'  # No translation file for en_US
         }
         self.locale_stack = list()
 

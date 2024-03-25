87eb4a8995b0197a7791c331120faa59e5f650c3
Keep Calm Stay Relax <101805283+iamnolimit@users.noreply.github.com>
2024-03-26 02:47:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,15 +21,15 @@
 import os
 from os import getenv
 
-TOKEN = os.getenv("TOKEN", config.get("token"))
-WORKERS = int(os.getenv("WORKERS", config.get("workers", 32)))
-ADMIN_LIST = os.getenv("ADMIN_LIST", config.get("admin_list", None))
+TOKEN = os.getenv("TOKEN", "")
+WORKERS = int(os.getenv("WORKERS", 32)))
+ADMIN_LIST = os.getenv("ADMIN_LIST", "")
 
 if isinstance(ADMIN_LIST, str):
     ADMIN_LIST = set(int(x) for x in ADMIN_LIST.split())
 
-OPEN_LOBBY = os.getenv("OPEN_LOBBY", config.get("open_lobby", True))
-ENABLE_TRANSLATIONS = os.getenv("ENABLE_TRANSLATIONS", config.get("enable_translations", False))
+OPEN_LOBBY = os.getenv("OPEN_LOBBY","")
+ENABLE_TRANSLATIONS = os.getenv("ENABLE_TRANSLATIONS", "")
 
 if isinstance(OPEN_LOBBY, str):
     OPEN_LOBBY = OPEN_LOBBY.lower() in ("yes", "true", "t", "1")
@@ -37,8 +37,8 @@ if isinstance(OPEN_LOBBY, str):
 if isinstance(ENABLE_TRANSLATIONS, str):
     ENABLE_TRANSLATIONS = ENABLE_TRANSLATIONS.lower() in ("yes", "true", "t", "1")
 
-DEFAULT_GAMEMODE = os.getenv("DEFAULT_GAMEMODE", config.get("default_gamemode", "fast"))
-WAITING_TIME = int(os.getenv("WAITING_TIME", config.get("waiting_time", 120)))
-TIME_REMOVAL_AFTER_SKIP = int(os.getenv("TIME_REMOVAL_AFTER_SKIP", config.get("time_removal_after_skip", 20)))
-MIN_FAST_TURN_TIME = int(os.getenv("MIN_FAST_TURN_TIME", config.get("min_fast_turn_time", 15)))
-MIN_PLAYERS = int(os.getenv("MIN_PLAYERS", config.get("min_players", 2)))
+DEFAULT_GAMEMODE = os.getenv("DEFAULT_GAMEMODE", "fast")
+WAITING_TIME = int(os.getenv("WAITING_TIME", 120))
+TIME_REMOVAL_AFTER_SKIP = int(os.getenv("TIME_REMOVAL_AFTER_SKIP", 20))
+MIN_FAST_TURN_TIME = int(os.getenv("MIN_FAST_TURN_TIME", 15))
+MIN_PLAYERS = int(os.getenv("MIN_PLAYERS", 2))
